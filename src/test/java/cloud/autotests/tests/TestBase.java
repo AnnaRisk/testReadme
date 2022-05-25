07f9ee2c25842936914393dc3119132fd05b5d47
24-002580\user <annarainchik@gmail.com>
2022-05-25 16:23:51 +0300

    add new

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -1,16 +1,20 @@
 package cloud.autotests.tests;
 
+import cloud.autotests.config.CredentialsConfig;
 import cloud.autotests.config.Project;
 import cloud.autotests.helpers.AllureAttachments;
 import cloud.autotests.helpers.DriverSettings;
 import cloud.autotests.helpers.DriverUtils;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 @ExtendWith({AllureJunit5.class})
@@ -19,6 +23,19 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DriverSettings.configure();
+
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://hh.ru");
+        Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("remote","selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
